0d32521d768018d5dfabdc7e64d40c021b34f0fe
Damian Osipiuk <osipiukd+git@gmail.com>
2021-03-12 15:45:57 +0100

    rerareadme, first test bootstrap

--- index.test.js
+++ index.test.js
@@ -1,5 +1,24 @@
+/* input:
+starting point x,y
+direction: N,W,S,E
+commands: string[]
+
+output:
+f, b - forward backward
+l, r - left, right
+*/
+
 describe("mars rover", () => {
-  it("should do bla", () => {
-    expect(true).toBe(false)
-  })
-})
+  it("should move forward by one tile", () => {
+    const x = 1;
+    const y = 1;
+    const direction = "N";
+    const commands = ["f"];
+
+    const expectedX = 1;
+    const expectedY = 2;
+
+
+    expect(true).toBe(false);
+  });
+});
