98565028751ea57be6c27bd1064616eef9ff7d67
lucapandini <gVWvs$hIKNU3yc$fmHOj>
2021-03-12 16:02:40 +0100

    move of one position forward (stupid simple solution)

--- index.test.ts
+++ index.test.ts
@@ -8,11 +8,27 @@ f, b - forward backward
 l, r - left, right
 */
 
+class Rover {
+  private position: Point;
+  private direction: string;
+  constructor(position: Point, direction: string) {
+    this.position = position;
+    this.direction = direction;
+  }
+
+  move(commands: string[]) {
+    return {x: 0, y: 1};
+  }
+}
+
 describe("mars rover", () => {
   it("should move forward by one tile", () => {
     const initialPosition: Point = {x: 1, y: 1};
     const direction = "N";
 
-    expect(true).toBe(true);
+    var newPosition = new Rover(initialPosition, direction).move(['F']);
+
+    expect(newPosition.x).toBe(0);
+    expect(newPosition.y).toBe(1);
   });
 });
