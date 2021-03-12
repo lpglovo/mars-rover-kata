c8152df108c99bdb87be0428f04915c2bed26103
Maurici Abad <maurici.abad@glovoapp.com>
2021-03-12 16:22:11 +0100

    mauri

--- index.test.ts
+++ index.test.ts
@@ -9,52 +9,65 @@ l, r - left, right
 */
 
 class Rover {
-  private position: Point;
-  private direction: string;
+  private position: Point
+  private direction: string
   constructor(position: Point, direction: string) {
-    this.position = position;
-    this.direction = direction;
+    this.position = position
+    this.direction = direction
   }
 
   move(commands: string[]) {
-    const command = commands[0];
+    const command = commands[0]
     switch (command) {
-      case 'f': return {x: 0, y: this.position.y + 1};
-      case 'b': return {x: 0, y: this.position.y - 1};
-      default: throw new Error('undefined command');
+      case 'f':
+        return { x: 0, y: this.position.y + 1 }
+      case 'b':
+        return { x: 0, y: this.position.y - 1 }
+      case 'r':
+        return { x: 0, y: this.position.y }
+      default:
+        throw new Error('undefined command')
     }
   }
 }
 
-describe("mars rover", () => {
-  it("should move forward by one tile", () => {
-    const initialPosition: Point = {x: 0, y: 0};
-    const direction = "N";
+describe('mars rover', () => {
+  it('should move forward by one tile', () => {
+    const initialPosition: Point = { x: 0, y: 0 }
+    const direction = 'N'
 
-    var newPosition = new Rover(initialPosition, direction).move(['f']);
+    var newPosition = new Rover(initialPosition, direction).move(['f'])
 
-    expect(newPosition.x).toBe(0);
-    expect(newPosition.y).toBe(1);
-  });
+    expect(newPosition.x).toBe(0)
+    expect(newPosition.y).toBe(1)
+  })
 
-  it("should move forward by one when position is 0,1", () => {
-    const initialPosition: Point = {x: 0, y: 1};
-    const direction = "N";
+  it('should move forward by one when position is 0,1', () => {
+    const initialPosition: Point = { x: 0, y: 1 }
+    const direction = 'N'
 
-    var newPosition = new Rover(initialPosition, direction).move(['f']);
+    var newPosition = new Rover(initialPosition, direction).move(['f'])
 
-    expect(newPosition.x).toBe(0);
-    expect(newPosition.y).toBe(2);
-  });
+    expect(newPosition.x).toBe(0)
+    expect(newPosition.y).toBe(2)
+  })
 
-  it("should move forward by one when position is 0,1", () => {
-    const initialPosition: Point = {x: 0, y: 1};
-    const direction = "N";
+  it('should move forward by one when position is 0,1', () => {
+    const initialPosition: Point = { x: 0, y: 1 }
+    const direction = 'N'
 
-    var newPosition = new Rover(initialPosition, direction).move(['b']);
+    var newPosition = new Rover(initialPosition, direction).move(['b'])
 
-    expect(newPosition.x).toBe(0);
-    expect(newPosition.y).toBe(0);
-  });
+    expect(newPosition.x).toBe(0)
+    expect(newPosition.y).toBe(0)
+  })
 
-});
+  it('should move turn right', () => {
+    const initialPosition: Point = { x: 0, y: 1 }
+    const direction = 'N'
+
+    var newPosition = new Rover(initialPosition, direction).move(['r'])
+
+    expect(newPosition).toEqual({ x: 0, y: 1 })
+  })
+})
