59484ecd2f1c758aaaf7a550450dea325fb03fbf
Angelo Sciarra <angelo.sciarra@glovoapp.com>
2021-03-19 17:39:39 +0100

    made it recursive

--- index.test.ts
+++ index.test.ts
@@ -1,27 +1,27 @@
-import { convert } from "./index";
+import {convert} from "./index";
 
 function testCase(input: number, output: string) {
-  return { input, output,};
+    return {input, output,};
 }
 
 const tests = [
-  testCase(1, "I"),
-  testCase(5, "V"),
-  testCase(10, "X"),
-  testCase(50, "L"),
-  testCase(100, "C"),
-  testCase(500, "D"),
-  testCase(1000, "M"),
-  testCase(2, "II"),
-  testCase(3, "III"),
-  testCase(20, "XX"),
-  testCase(2000, "MM"),
-  testCase(26, "XXVI"),
-  testCase(4, "IV"),
+    testCase(1, "I"),
+    testCase(5, "V"),
+    testCase(10, "X"),
+    testCase(50, "L"),
+    testCase(100, "C"),
+    testCase(500, "D"),
+    testCase(1000, "M"),
+    testCase(2, "II"),
+    testCase(3, "III"),
+    testCase(20, "XX"),
+    testCase(2000, "MM"),
+    testCase(26, "XXVI"),
+    testCase(4, "IV"),
 ];
 
 describe("roman numbers", () => {
-  it.each(tests)("Should return proper output", (test) => {
-    expect(convert(test.input)).toBe(test.output);
-  });
+    it.each(tests)("Should return proper output", (test) => {
+        expect(convert(test.input)).toBe(test.output);
+    });
 });
--- index.ts
+++ index.ts
@@ -1,29 +1,39 @@
 const symbols: Record<number, string> = {
-  1: "I",
-  4: "IV",
-  5: "V",
-  10: "X",
-  50: "L",
-  100: "C",
-  500: "D",
-  1000: "M",
+    1: "I",
+    4: "IV",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M",
 };
 
-const sortFn = (a: number, b: number) => {
-  return b-a;
-};
+const sortFn = (a: number, b: number) => b - a
+
+const conversionLoop = (input: number, mappedNumbers: number[], runningRoman: string): string => {
+
+    if (input == 0) return runningRoman
+
+    let arabicNumber = mappedNumbers[0];
+
+    if (arabicNumber <= input) {
+        return conversionLoop(
+            input - arabicNumber,
+            mappedNumbers,
+            runningRoman + symbols[arabicNumber],
+        )
+    } else {
+        return conversionLoop(
+            input,
+            mappedNumbers.slice(1),
+            runningRoman
+        )
+    }
+}
+
+const mappedArabicNumbers: number[] = Object.keys(symbols).map(Number).sort(sortFn);
 
 export function convert(number: number): string {
-  let output = "";
-  while (number > 0) {
-    const kNumbers = Object.keys(symbols).map(Number).sort(sortFn);
-    for (const kNumber of kNumbers) {
-      if (kNumber <= number) {
-        output += symbols[kNumber];
-        number -= kNumber;
-        break;
-      }
-    }
-  }
-  return output;
+    return conversionLoop(number, mappedArabicNumbers, "")
 }
