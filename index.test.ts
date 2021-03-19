5c0d371a21efe82d90c488752ea42fde5f443231
Damian Osipiuk <osipiukd+git@gmail.com>
2021-03-19 15:35:53 +0100

    first test pass

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,14 @@
+import { convert } from "./index";
+
+const tests = [
+  {
+    input: 1,
+    output: "I",
+  },
+];
 
 describe("roman numbers", () => {
-  
+  it.each(tests)("Should return proper output", (test) => {
+    expect(convert(test.input)).toBe(test.output);
+  });
 });
